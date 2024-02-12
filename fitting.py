5331d1960f3b93737a99fcca0e7128fba6fccde1
IA3005 <81386632+IA3005@users.noreply.github.com>
2024-02-12 16:33:58 +0100

    Update fitting.py

--- fitting.py
+++ fitting.py
@@ -1,5 +1,4 @@
 import numpy as np
-from preprocess_ssvep import ExoSkeleton
 from tWishart import t_wish_est, t_wishart_rvs
 from joblib import Parallel, delayed
 from tqdm import tqdm
@@ -10,27 +9,44 @@ from scipy.stats import wishart,kstest
 
 def cdf(samples,x):
     """
+    computes the empirical cumulative density function of samples at the quantile x i.e. proportion of samples that are <= x.
+    
     Parameters
     ----------
-    samples : array or list of floats
+    samples : array or list of floats.
+        random floats
     x : float
+        quantile
 
     Returns
     -------
     float
-        the empirical cdf for samples at x i.e. proportion of samples that are <= x.
+        the empirical cdf of samples at x.
     """
     card=0
     K=len(samples)
     for k in range(K):
-        if samples[k]<x:
+        if samples[k]<=x:
             card+=1
     return card/K
 
 
 def eeg_covs(data,tmin=0,delta_t=3,resampling=256):
     """
-     Returns
+    extracts the covariance matrices of all the subjects of the dataset.
+    
+    Parameters
+    ----------
+    data : dict
+        dictionary containing the trials and the labels of each subject.
+    tmin : float, optional
+        offset of trials (default 2).
+    delta_t : float, optional
+        duration of trials (default 3).
+    resampling : float, optional
+        resampling frequency (Hz) (default 256). 
+        
+    Returns
     -------
     cov_data : dict
         covariance matrices (SCM) and labels for all the subjects of the dataset.
@@ -62,17 +78,44 @@ def eeg_covs(data,tmin=0,delta_t=3,resampling=256):
 def fit_eeg(quantity_to_fit,cov_data,selected_subjs,delta_t,resampling,
             df={1:20, 2:20, 3:20, 4:20},iters=500,n_samples=100000,n_jobs=-1):
     """
-
+    performs the fitting of real EEG data with the Wishart and the t-Wishart distributions.
 
     Parameters
     ----------
-    x : TYPE
-        DESCRIPTION.
+    quantity_to_fit : str
+        Quantity on which we perform fitting. For example, if `trace`, the fitting of the trace of samples is considered.
+    cov_data : dict
+        Dictionary of covariance matrice and labels of each subject from the SSVEP-EEG dataset
+    selected_subjs : list
+        List of the chosen subjects on which fitting is performed.
+    delta_t : float, optional
+        duration of trials (default 3).
+    resampling : float, optional
+        resampling frequency (Hz) (default 256). 
+    df : dict, optional
+        Degrees of freedom for each class (default {1:20, 2:20, 3:20, 4:20})
+    iters : int, optional 
+        Number of quantiles per class (defaut 500)
+    n_samples : int,optional
+        Number of Wishart/t-Wishart samples drawn to fit real data (default 100000)
+    n_jobs : int,optional
+        Number of jobs (default -1).
 
     Returns
     -------
-    TYPE
-        DESCRIPTION.
+    xs : dict
+        Quantiles for each class.
+    eeg_cdf : dict
+        Empirical cumulative density function of EEG samples for each class 
+        (for example, if quantity_to_fit is `norm`, the samples are the norms of the extracted covariance matrices)  
+    wishart_cdf : dict
+        Cumulative density function of Wishart samples for each class (the center of each class is estimated using the Wishart estimator)
+    t_wishart_cdf : dict
+        Cumulative density function of t-Wishart samples for each class (the center of each class is estimated using the MLE)
+    tests_W : dict
+        Results of Kolmogorov-Smirnov fitting test (statistic and p-value) with the Wishart distribution for each class
+    tests_tW : dict
+        Results of Kolmogorov-Smirnov fitting test (statistic and p-value) with the t-Wishart distribution for each class
 
     """
     parallel = Parallel(n_jobs=n_jobs, verbose=0)

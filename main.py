832aedda3234416d951278b5569b78a4e568af6a
IA3005 <81386632+IA3005@users.noreply.github.com>
2024-02-12 16:07:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,22 +2,24 @@ from preprocess_ssvep import ExoSkeleton
 from fitting import eeg_covs, fit_eeg
 import matplotlib.pyplot as plt
 
-tmin = 2
-delta_t = 3 
-resampling = 256
-selected_subjs = [12]
-quantity_to_fit = "norm"
-df = {1:24, 2:20, 3:28, 4:41}
+tmin = 2 #offset for trials 
+delta_t = 3 #considered duration for trials
+resampling = 256 #resampling frequency
+selected_subjs = [12] #list of selected subjects from the twelve subjects of the dataset
+quantity_to_fit = "norm" #change into "det" or "trace" if you want to perform the fitting of the determinant or the trace, respectively
+df = {1:24, 2:20, 3:28, 4:41} #degrees of freedom for each class
 n_jobs = -1
 
-
+#1. Load the dataset
 data = ExoSkeleton(tmin,delta_t,resampling).load_all_data()
- 
+
+#2. Extract covariance matrices 
 cov_data = eeg_covs(data,tmin,delta_t,resampling)
- 
+
+#3. Fitting
 xs,eeg_cdf,wishart_cdf,t_wishart_cdf,tests_W , tests_tW = fit_eeg(quantity_to_fit,cov_data,selected_subjs,delta_t,resampling, df=df,n_jobs=n_jobs)
 
-
+#4. Plot results of the fitting for each class
 for k in range(1,len(xs)+1):
     plt.plot(xs[k],eeg_cdf[k],label="eeg samples",linestyle="solid")
     plt.plot(xs[k],t_wishart_cdf[k],label="$t$-Wishart samples of df="+str(df[k]),linestyle="dotted")
